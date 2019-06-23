84ab9eaa94ea4fdc112416865daed776f77947bd
Nikita Gorokhov <bkng@bk.ru>
2019-06-23 14:34:12 +0300

    df model

--- modeling/color_pred/model.py
+++ modeling/color_pred/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
+from scipy.sparse import hstack
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
@@ -48,19 +49,6 @@ AUCs = []
 # # X_train_d = pca.transform(X_train_d.toarray())
 # # X_test_d = pca.transform(X_test_d.toarray())
 #
-# model_perceptron = Perceptron(max_iter=200,
-#                               penalty=None,
-#                               early_stopping=True,
-#                               validation_fraction=0.05,
-#                               random_state=228)
-# model_perceptron.fit(X_train_d, y_train)
-# p_train = model_perceptron.predict(X_train_d)
-# p_test = model_perceptron.predict(X_test_d)
-#
-# AUCs += [[roc_auc_score(y_train, p_train), roc_auc_score(y_test, p_test)]]
-#
-# print(AUCs)
-#
 #
 # model_logit = LogisticRegression(penalty='l1',
 #                                  solver='liblinear',
@@ -92,67 +80,54 @@ AUCs = []
 
 print('lgb on text')
 
-# params = {
-#     'max_bin': 7,
-#     'boost_from_average': 'false',
-#     'boost': 'gbdt',
-#     'feature_fraction': 0.85,
-#     'bagging_fraction': 0.8,
-#     'learning_rate': 0.05,
-#     'max_depth': 25,
-#     'metric': 'auc',
-#     'min_data_in_leaf': 1,
-#     'min_sum_hessian_in_leaf': 0,
-#     'num_leaves': 101,
-#     'num_threads': 8,
-#     'lambda_l1': 0,
-#     'lambda_l2': 0,
-#     'tree_learner': 'serial',
-#     'objective': 'binary'
-#
-# }
-#
-# to_train = lgb.Dataset(X_train_d, y_train)
-# to_val = lgb.Dataset(X_test_d, y_test)
-# model_lgb = lgb.train(params,
-#                       to_train,
-#                       valid_sets=[to_train, to_val],
-#                       num_boost_round=10000,
-#                       verbose_eval=100,
-#                       early_stopping_rounds=100)
-# p_train = model_lgb.predict(X_train_d)
-# p_test = model_lgb.predict(X_test_d)
-#
-# AUCs += [[roc_auc_score(y_train, p_train), roc_auc_score(y_test, p_test)]]
-#
-# print(AUCs)
-#
-#
-#
-# pca = PCA(n_components=1000,
-#           random_state=228)
-# pca.fit(X_train_d.toarray())
-# X_train_d = pca.transform(X_train_d.toarray())
-# X_test_d = pca.transform(X_test_d.toarray())
+params = {
+    'max_bin': 7,
+    'boost_from_average': 'false',
+    'boost': 'gbdt',
+    'feature_fraction': 0.85,
+    'bagging_fraction': 0.8,
+    'learning_rate': 0.05,
+    'max_depth': 25,
+    'metric': 'auc',
+    'min_data_in_leaf': 1,
+    'min_sum_hessian_in_leaf': 0,
+    'num_leaves': 101,
+    'num_threads': 8,
+    'lambda_l1': 0,
+    'lambda_l2': 0,
+    'tree_learner': 'serial',
+    'objective': 'binary'
 
-model_perceptron = Perceptron(max_iter=200,
-                              penalty=None,
-                              early_stopping=True,
-                              validation_fraction=0.05,
-                              random_state=228)
-model_perceptron.fit(X_train_f, y_train)
-p_train = model_perceptron.predict(X_train_f)
-p_test = model_perceptron.predict(X_test_f)
+}
 
-AUCs += [[roc_auc_score(y_train, p_train), roc_auc_score(y_test, p_test)]]
+to_train = lgb.Dataset(X_train_d, y_train)
+to_val = lgb.Dataset(X_test_d, y_test)
+model_lgb = lgb.train(params,
+                      to_train,
+                      valid_sets=[to_train, to_val],
+                      num_boost_round=10000,
+                      verbose_eval=100,
+                      early_stopping_rounds=100)
+p_train_d = model_lgb.predict(X_train_d)
+p_test_d = model_lgb.predict(X_test_d)
+
+AUCs += [[roc_auc_score(y_train, p_train_d), roc_auc_score(y_test, p_test_d)]]
 
 print(AUCs)
+
+print('lgb on feat')
+
+# pca = PCA(n_components=1000,
+#           random_state=228)
+# pca.fit(X_train_f.toarray())
+# X_train_d = pca.transform(X_train_f.toarray())
+# X_test_d = pca.transform(X_test_f.toarray())
 
 
 # model_logit = LogisticRegression(penalty='l1',
 #                                  solver='liblinear',
-#                                  C=1,
-#                                  tol=1e-3,
+#                                  C=0.1,
+#                                  tol=1e-4,
 #                                  max_iter=100,
 #                                  random_state=228)
 # model_logit.fit(X_train_f, y_train)
@@ -164,11 +139,10 @@ print(AUCs)
 # print(AUCs)
 #
 #
-# model_forest = RandomForestClassifier(n_estimators=100,
+# model_forest = RandomForestClassifier(n_estimators=200,
 #                                       oob_score=True,
-#                                       max_depth=100,
-#                                       min_samples_leaf=1,
-#                                       verbose=1,
+#                                       max_depth=13,
+#                                       min_samples_leaf=2,
 #                                       random_state=228)
 # model_forest.fit(X_train_f, y_train)
 # p_train = model_forest.predict_proba(X_train_f)[:, 1]
@@ -177,38 +151,53 @@ print(AUCs)
 # AUCs += [[roc_auc_score(y_train, p_train), roc_auc_score(y_test, p_test)]]
 #
 # print(AUCs)
-#
-# params = {
-#     'max_bin': 7,
-#     'boost_from_average': 'false',
-#     'boost': 'gbdt',
-#     'feature_fraction': 0.85,
-#     'bagging_fraction': 0.8,
-#     'learning_rate': 0.05,
-#     'max_depth': 25,
-#     'metric': 'auc',
-#     'min_data_in_leaf': 1,
-#     'min_sum_hessian_in_leaf': 0,
-#     'num_leaves': 101,
-#     'num_threads': 8,
-#     'lambda_l1': 0,
-#     'lambda_l2': 0,
-#     'tree_learner': 'serial',
-#     'objective': 'binary'
-#
-# }
-#
-# to_train = lgb.Dataset(X_train_f, y_train)
-# to_val = lgb.Dataset(X_test_f, y_test)
-# model_lgb = lgb.train(params,
-#                       to_train,
-#                       valid_sets=[to_train, to_val],
-#                       num_boost_round=10000,
-#                       verbose_eval=100,
-#                       early_stopping_rounds=100)
-# p_train = model_lgb.predict(X_train_f)
-# p_test = model_lgb.predict(X_test_f)
-#
-# AUCs += [[roc_auc_score(y_train, p_train), roc_auc_score(y_test, p_test)]]
-#
-# print(AUCs)
+
+params = {
+    'max_bin': 255,
+    'boost_from_average': 'false',
+    'boost': 'gbdt',
+    'feature_fraction': 1,
+    'bagging_fraction': 0.8,
+    'learning_rate': 0.05,
+    'max_depth': 25,
+    'metric': 'auc',
+    'min_data_in_leaf': 1,
+    'min_sum_hessian_in_leaf': 0,
+    'num_leaves': 101,
+    'num_threads': 8,
+    'lambda_l1': 0,
+    'lambda_l2': 0,
+    'tree_learner': 'serial',
+    'objective': 'binary'
+}
+
+to_train = lgb.Dataset(X_train_f, y_train)
+to_val = lgb.Dataset(X_test_f, y_test)
+model_lgb = lgb.train(params,
+                      to_train,
+                      valid_sets=[to_train, to_val],
+                      num_boost_round=1000,
+                      verbose_eval=10,
+                      early_stopping_rounds=20)
+p_train_f = model_lgb.predict(X_train_f)
+p_test_f = model_lgb.predict(X_test_f)
+
+AUCs += [[roc_auc_score(y_train, p_train_f), roc_auc_score(y_test, p_test_f)]]
+
+print(AUCs)
+
+X_train_df = pd.DataFrame({'d': p_train_d,
+                           'f': p_train_f})
+X_test_df = pd.DataFrame({'d': p_test_d,
+                          'f': p_test_f})
+print(X_train_df)
+
+model_logit = LogisticRegression()
+model_logit.fit(X_train_df, y_train)
+p_train = model_logit.predict_proba(X_train_df)[:, 1]
+p_test = model_logit.predict_proba(X_test_df)[:, 1]
+
+
+AUCs += [[roc_auc_score(y_train, p_train), roc_auc_score(y_test, p_test)]]
+
+print(AUCs)
